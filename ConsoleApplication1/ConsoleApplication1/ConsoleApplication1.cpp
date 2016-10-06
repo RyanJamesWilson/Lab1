1b116953bb43573d313ff43cd92f46f664d2f5e2
RWilson <Rwilson0451@gmail.com>
2016-10-06 15:41:36 +0100

    Commit1
    More commitment

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "I hate myself" << endl;
+
+    return 0;
+}
+
